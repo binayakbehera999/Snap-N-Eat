63c4562630e09b473a5de4dbf8976660491077ad
mahesdwivedi <mahesdwivedi@gmail.com>
2020-07-31 19:36:13 +0530

    add calorie data for food

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<DashBoardProvider>(
+return ChangeNotifierProvider<DashBoardProvider>( 
       create: (context) => DashBoardProvider(),
       child: MaterialApp(
         theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Poppins'),
